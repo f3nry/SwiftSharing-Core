6c624e63cabef20d5c26c456328256ef8c4f69b0
Paul Henry <paulhenry@mphwebsystems.com>
2011-03-21 02:30:16 -0500

    Added database config.

---
+++ modules/database/config/database.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+(
+        'default' => array
+        (
+                'type'       => 'mysql',
+                'connection' => array(
+                        /**
+                         * The following options are available for MySQL:
+                         *
+                         * string   hostname     server hostname, or socket
+                         * string   database     database name
+                         * string   username     database username
+                         * string   password     database password
+                         * boolean  persistent   use persistent connections?
+                         *
+                         * Ports and sockets may be appended to the hostname.
+                         */
+                        'hostname'   => 'localhost',
+                        'database'   => 'swshare_swift',
+                        'username'   => 'root',
+                        'password'   => 'patter5Dress',
+                        'persistent' => TRUE,
+                ),
+                'table_prefix' => '',
+                'charset'      => 'utf8',
+                'caching'      => FALSE,
+                'profiling'    => TRUE,
+        )
+);
