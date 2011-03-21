52435ad67e0c5ea45210017ef04ba59b8a7411e2
Paul Henry <paulhenry@mphwebsystems.com>
2011-03-21 03:08:51 -0500

    Another git test.

--- application/classes/controller/members.php
+++ application/classes/controller/members.php
@@ -7,7 +7,7 @@ class Controller_Members extends Controller_App {
         $this->_requireAuth();
 
         $post = $this->request->post();
-        $pager = Util_Pager::setup($this->request->param('page_number'), 15, 'myMembers WHERE email_activated = 0', '/members/search');
+        $pager = Util_Pager::setup($this->request->param('page_number'), 15, 'myMembers WHERE email_activated != \'0\'', '/members/search');
 
         $this->template->totalMembers = Model_Member::getTotalCount();
         $this->template->members = Model_Member::doSearch(@$post['searchField'], $pager);
