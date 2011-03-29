10d858ae4532aabf9915b976186d161a2dc3791b
Paul Henry <paulhenry@mphwebsystems.com>
2011-03-28 23:01:40 -0500

    Added Facebook module.

---
+++ modules/facebook/config/facebook.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'app_id'          => '204162316278860',
+	'api_key'         => '126f06969d1bb978326140312e4ef7bf',
+	'secret'          => '59a48a88a5d265d832b5d626494eb619',
+);
