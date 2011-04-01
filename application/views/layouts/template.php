cebea168cee434c291944f2a597e09f7742100ec
Paul Henry <paulhenry@mphwebsystems.com>
2011-04-01 00:38:11 -0500

    April Fools Day.

--- application/views/layouts/template.php
+++ application/views/layouts/template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html >
     <head>
-        <title>SwiftSharing</title>
+        <title>SlowSharing</title>
         <link rel="stylesheet" type="text/css" href="/content/css/new.css"/>
         <link rel="stylesheet" type="text/css" href="/content/css/screen.css" />
         <?php if(is_array(@$styles)): ?>
@@ -70,7 +70,7 @@
                 <?php endif; ?>
                     <div id="footer">
                     <div style="float:left;">
-                        <span>SwiftSharing</span> | <a href="/help">Help</a> | <a href="/about">About</a> | <a href="/privacy">Privacy</a>
+                        <span>SlowSharing</span> | <a href="/help">Help</a> | <a href="/about">About</a> | <a href="/privacy">Privacy</a>
                     </div>
                         <div style="float:right;">
                             <div style="width:250px;float:left;height:62px;">
