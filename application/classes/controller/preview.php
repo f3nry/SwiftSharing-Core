2e87bb1ef02fd89ca790ee800048b4789f24ce3c
Paul Henry <paul@swiftsharing.net>
2011-06-08 23:13:24 -0500

    Preview page for 3.0.

---
+++ application/classes/controller/preview.php
@@ -0,0 +1,11 @@
+<?php
+
+class Controller_Preview extends Controller_App {
+	public $layout = false;
+	
+	public function action_index() {
+		
+	}
+}
+
+?>
---
+++ application/views/preview/index.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="/content/css/preview/style.css" />
+<title>SwiftSharing 3.0</title>
+</head>
+<div id="container">
+	<div id="top">
+		<div class="logo">
+			<img src="/content/images/preview/logo.png"></img>
+		</div>
+		</div>
+	<div id="left">
+		<div class="feature-body"> 
+		<div class="feature-img"> 
+		<img src="/content/images/preview/design.png" style='width:160px;height:150px'/> 
+		</div> 
+		<div class="feature-text"> 
+		<h2>Design</h2>
+		<p>SwiftSharing 3.0 brings in a more logical design, making it easier for you to share.</p>
+		</div> 
+		</div>
+		<div class="feature-body"> 
+		<div class="feature-img"> 
+		<img src="/content/images/preview/sharing.png" style='width:160px;height:150px'/> 
+		</div> 
+		<div class="feature-text"> 
+		<h2>Sharing: Simplified</h2>
+		<p>Sharing with SwiftSharing 3.0 is designed to be simpler and easier to do.</p>
+		</div> 
+		</div>
+		<div class="feature-body"> 
+		<div class="feature-img"> 
+		<img src="/content/images/preview/photos.png" style='width:160px;height:150px'/> 
+		</div> 
+		<div class="feature-text"> 
+		<h2>Photos, Create Albums</h2>
+		<p>Share photos with friends even easier, and put them into albums with SwiftSharing 3.0.</p>
+		</div> 
+		</div>
+		<div class="feature-body"> 
+		<div class="feature-img"> 
+		<img src="/content/images/preview/realtime.png" style='width:160px;height:150px'/> 
+		</div> 
+		<div class="feature-text"> 
+		<h2>More Realtime Feeds</h2>
+		<p>Feeds become even more realtime. See what friends are doing, as soon as it happens with SwiftSharing 3.0.</p>
+		</div> 
+		</div>
+		<br/><br/><br/>
+	</div>
+	<div id="right">
+		<h1>Get Access when 3.0 releases!</h1>
+		<p class="right">Want access to SwiftSharing 3.0?<p>
+		<h3>We will let everyone know when and how they can get access to SwiftSharing 3.0 in about a week.</h3>
+		</div>
+</div>
