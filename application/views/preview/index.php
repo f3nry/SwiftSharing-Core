da7ef57c4d9eff59b1853c41efb771e2343e7683
Paul Henry <paul@swiftsharing.net>
2011-06-08 23:59:08 -0500

    Added twitter and facebook buttons.

--- application/views/preview/index.php
+++ application/views/preview/index.php
@@ -46,11 +46,16 @@
 		<p>Feeds become even more realtime. See what friends are doing, as soon as it happens with SwiftSharing 3.0.</p>
 		</div> 
 		</div>
-		<br/><br/><br/>
 	</div>
 	<div id="right">
 		<h1>Get Access when 3.0 releases!</h1>
 		<p class="right">Want access to SwiftSharing 3.0?<p>
 		<h3>We will let everyone know when and how they can get access to SwiftSharing 3.0 in about a week.</h3>
+		<div class="facebook">
+		<iframe src="http://www.facebook.com/plugins/like.php?app_id=204162316278860&amp;href=http%3A%2F%2Fswiftsharing.net%2Fpreview&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>
+		</div>
+		<div class="twitter">
+		<a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="SwiftSharing">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+		</div>
 		</div>
 </div>
