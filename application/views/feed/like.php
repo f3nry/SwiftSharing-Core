bd3d91603e1a090a67d1e5e02d28e3215643083b
Paul Henry <paul@swiftsharing.net>
2011-07-26 02:19:56 -0500

    Fixed unsupported short tag.

--- application/views/feed/like.php
+++ application/views/feed/like.php
@@ -1,14 +1,14 @@
-<div id="thumbsup_<?= $id ?>" class="thumbsup_template_mini-thumbs">
+<div id="thumbsup_<?php echo $id ?>" class="thumbsup_template_mini-thumbs">
 		<form method="post">
-				<input type="hidden" name="thumbsup_id" value="<?= $id ?>" />
-				<input type="hidden" name="type" value="<?= $type ?>" />
+				<input type="hidden" name="thumbsup_id" value="<?php echo $id ?>" />
+				<input type="hidden" name="type" value="<?php echo $type ?>" />
 
 				<span class="thumbsup_hide">Score:</span>
 				<?php if($delete): ?>
-						<a href="javascript:void" onclick="deleteBlab(<?=$id ?><?php if($type == 'ALBUM'): ?>, 'ALBUM'<?php endif; ?>)" style="float:right;padding-left:5px;"><img src="/content/images/notifications/icon_close.png" width="16" height="16" /></a>
+						<a href="javascript:void" onclick="deleteBlab(<?php echo$id ?><?php if($type == 'ALBUM'): ?>, 'ALBUM'<?php endif; ?>)" style="float:right;padding-left:5px;"><img src="/content/images/notifications/icon_close.png" width="16" height="16" /></a>
 				<?php endif; ?>
 				<input class="vote_up" name="thumbsup_rating" value="+1" type="submit" title="Vote up" />
 				<input class="vote_down" name="thumbsup_rating" value="-1" type="submit" title="Vote down" />
-				<strong class="votes_balance"><?= $likes ?></strong>
+				<strong class="votes_balance"><?php echo $likes ?></strong>
 		</form>
 </div>
--- application/views/profile/albums.php
+++ application/views/profile/albums.php
@@ -2,20 +2,20 @@
 <div class="albums">
 	<?php if(!empty($collections)): ?>
 		<?php foreach($collections as $collection): ?>
-			<div class="album" id="album_<?= $collection->id ?>">
+			<div class="album" id="album_<?php echo $collection->id ?>">
 				<?php $i = 0; ?>
 					<div class="album_photo">
-						<a href="/photos/<?= $collection->first() ?>">
+						<a href="/photos/<?php echo $collection->first() ?>">
 							<?php foreach($collection->getPhotos(false) as $photo): ?>
-							<img class="album_<?=$i ?>" src="<?php echo $photo->getUrl() ?>" width="180" height="180"/>
+							<img class="album_<?php echo $i ?>" src="<?php echo $photo->getUrl() ?>" width="180" height="180"/>
 							<?php $i++; if($i >= 3) { break; } ?>
 							<?php endforeach; ?>
 							<div style="clear:both;"></div>
 						</a>
 					</div>
-				<span class="album_name"><?= $collection->getName() ?></span>
+				<span class="album_name"><?php echo $collection->getName() ?></span>
 				<span class="likes">
-					<?= Model_Like::generateLikeBox($collection->id, $collection->likes, true, 'ALBUM'); ?>
+					<?php echo Model_Like::generateLikeBox($collection->id, $collection->likes, true, 'ALBUM'); ?>
 				</span>
 			</div>
 		<?php endforeach; ?>
